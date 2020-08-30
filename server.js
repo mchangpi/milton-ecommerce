ac10f4fb6071f774ccc2f7ca7190e3a3574e8f30
Milton Chang <mchangpi@gmail.com>
2020-08-30 09:37:20 +0800

    61. How Middleware Works

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app);
 
+app.use((req, resp, next) => {
+  console.log("In the middleware");
+  next();
+});
+
+app.use((req, resp, next) => {
+  console.log("In another middleware");
+  resp.json("Hello from Express");
+});
+
 server.listen(3000, () => {
   console.log("Node is listening on port 3000...");
 });
