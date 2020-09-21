e5d4b375e9dbe720dc5976da436f5556068ba6b7
Milton Chang <mchangpi@gmail.com>
2020-09-21 09:47:37 +0800

    161. Adding a One-To-Many Relationship

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 app.use("/", express.static("public"));
@@ -22,8 +24,13 @@ app.use("/admin", adminData.router);
 
 app.use("/", errorController.get404);
 
+// constraints: Should on update and on delete constraints be enabled on the foreign key.
+// onDelete: SET NULL if foreignKey allows nulls, CASCADE if otherwise
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
-  .sync()
+  .sync({ force: true }) // adds a DROP TABLE IF EXISTS
   .then((result) => {
     // console.log("seq sync result ", result);
     app.listen(3000, () => {
--- models/product.js
+++ models/product.js
@@ -1,21 +1,26 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../util/database");
 
-const User = sequelize.define("user", {
+const Product = sequelize.define("product", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true,
   },
-  name: {
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
     type: Sequelize.STRING,
     allowNull: false,
   },
-  email: {
+  description: {
     type: Sequelize.STRING,
     allowNull: false,
   },
 });
 
-module.exports = User;
+module.exports = Product;
