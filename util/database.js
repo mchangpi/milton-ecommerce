0c9803d48843ea908a8f5a33586426ae0b61e56e
Milton Chang <mchangpi@gmail.com>
2020-11-15 22:08:54 +0800

    User db4free as cloud MySQL

--- util/database.js
+++ util/database.js
@@ -1,15 +1,16 @@
-const mysql = require("mysql2");
+//const mysql = require("mysql2");
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const [host, user, password, port] = [
-  "sql12.freemysqlhosting.net",
-  "sql12366529",
+const [host, user, password, dbname, port] = [
+  "db4free.net",
+  "mchangpi",
   process.env.DB_PASS,
+  "mariadb4free",
   3306,
 ];
 
-const sequelize = new Sequelize(user, user, password, {
+const sequelize = new Sequelize(dbname, user, password, {
   host: host,
   dialect: "mysql",
   port: port,
